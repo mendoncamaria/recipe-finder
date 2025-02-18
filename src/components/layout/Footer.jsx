cfcf791a915292dbc19fee478194a823ce173874
Maria Mendonca <maria.mendonca156@gmail.com>
2025-02-18 16:28:59 +0530

    Updated footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,31 +1,12 @@
-import React from "react";
+import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-gray mt-auto w-full h-full py-8 ">
-      <div className="container flex items-center justify-between gap-8">
-        <p>©{new Date().getFullYear()} CookRecipes - All rights reserved.</p>
-        <p>
-          Made by{" "}
-          <a
-            target="_blank"
-            className="font-bold"
-            // href="https://sujoykh.vercel.app"
-          >
-            Maria
-          </a>
-          , build with <span className="font-bold">React.js,</span>{" "}
-          <span className="font-bold">Tailwind CSS</span>,{" "}
-          <a
-            href="https://spoonacular.com/food-api/docs"
-            target="_blank"
-            className="font-bold"
-          >
-            Spoonacular API
-          </a>
-          .
-        </p>
-      </div>
+    <footer className="bg-black text-white text-center py-4">
+      &copy;{new Date().getFullYear()}{' '}
+      <a href="https://maria-mendonca.vercel.app/" target="_self">
+        Maria Mendonca
+      </a>
     </footer>
   );
 }
